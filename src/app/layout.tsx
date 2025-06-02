e27b82d185233a0c63fd139889525291ea29b04d
Wisley56 <fwisley56@gmail.com>
2025-06-02 00:56:05 -0300

    ajuste para ficar personalizado na guia do navegador

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
-
+import type { Metadata } from 'next';
 import Navbar from "@/components/layout/Navbar";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: 'CineControl WVision',
+  description: 'Sua plataforma para gerenciar filmes, salas e sessões de cinema.',
+  };
 
 export default function RootLayout({
   children,
